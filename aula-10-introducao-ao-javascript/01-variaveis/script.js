9db61305db2893f97a342812b12064a0bcea955b
lopes7890 <lopesallison414@gmail.com>
2026-05-25 19:37:59 -0300

    o2-operadores

--- aula-10-introducao-ao-javascript/01-variaveis/script.js
+++ aula-10-introducao-ao-javascript/01-variaveis/script.js
@@ -1,18 +1,43 @@
-// Variáveis podem ser declaradas de três formas: VAR, LET e CONST
-
-// VAR no geral, evite seu uso, pode não ser muito seguro.
-
-// LET utilze quando for necessário reatribuir o valor da variável
-
-// CONST utilize quando não precisar reatribuir o valor da variável
-
+// Javascript
+// VARIÁVEIS
+/* Variáveis podem ser declaradas de 3 formas:
+VAR, LET e CONST */
+// VAR: No geral, evite o seu uso, pode não ser muito seguro.
+// LET: Utilize quando for necessário reatribuir o valor da variável.
+// CONST: Utilize quando NÃO precisar reatribuir o valor da variável.
 // VAR:
-var nome = "Allison";
+var nome = "Diego";
 nome = "João";
-
+var nome = "José";
 // LET:
 let cidade = "Registro";
 cidade = "Sete Barras";
-
+// let cidade -> isso não pode
 // CONST:
 const user = "pedro@email.com";
+// user = "pedro@gmail.com" -> isso não pode
+// const user = "" -> não poderia
+
+// FUNÇÕES DO ELEMENTO WINDOW (navegador)
+// Disparando uma janela de alerta no site
+window.alert("Bem-vindo aos de Javascript!");
+// Disparando uma janela de confirmação
+const confirmacao = window.confirm("Vamos começar?");
+if (confirmacao == true) {
+  // Dispara uma janela de entrada de dados
+  const nome = window.prompt("Qual é o seu nome?");
+  const idade = window.prompt("Quantos anos você tem?")
+  // Escrevendo no site
+  document.writeln("Olá " + nome + "!"); // Concatenação
+  // Concatenação - Template Strings
+  // document.write("Você tem " + idade + " anos")
+  document.writeln(`Você tem ${idade} anos!`)
+}
+// CRIE UM SITE COM UM SCRIPT QUE PERGUNTE A IDADE DO USUÁRIO. SE A IDADE FOR MAIOR QUE 18, ELE MOSTRA NO SITE: "VOCÊ É MAIOR DE IDADE.", SE NÃO ELE MOSTRA: "VOCÊ É MENOR DE IDADE."
+const idade = Number(window.prompt("Digite sua idade: "));
+
+if (idade <= 18){
+  document.writeln("Maior de idade")
+} else {
+  document.writeln("Menor de idade")
+}
---
+++ aula-10-introducao-ao-javascript/02-operadores/script.js
@@ -0,0 +1,42 @@
+let n1 = Number(window.prompt("Digite um número: "));
+
+let n2 = Number(window.prompt("Digite outro número: "));
+
+const soma = n1 + n2;
+
+window.alert(`A soma é ${soma}`);
+
+//MULTIPLICAÇÃO
+
+let mult = n1 * n2;
+document.writeln(`<br> A Multiplicação de n1 e n2 é igual ${mult}`);
+
+//SUBTRAÇÃO
+
+let sub = n1 - n2;
+document.writeln(`<br> A Subtração de n1 e n2 é igual ${sub}`);
+
+//POTENCIA
+
+let pot = n1 ** n2;
+document.writeln(`<br> A Potencia de n1 e n2 é igual ${pot}`);
+
+//DIVISAO REAL
+
+let divr = n1 / n2;
+document.writeln(`<br> A Divisão real de n1 e n2 é igual ${divr}`);
+
+//RESTO DA DIVISAO 
+
+let rdiv = n1 % n2;
+document.writeln(`<br> O Resto da divisão de n1 e n2 é igual ${rdiv}`);
+
+// OPERADOR DE ATRIBUIÇÃO
+
+n1 += n2
+document.writeln(`<br> ${n1}`);
+n1 += n2
+document.writeln(`<br> ${n1}`);
+n1 += n2
+document.writeln(`<br> ${n1}`);
+
